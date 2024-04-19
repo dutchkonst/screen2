b87872827432d16b18237e66638bfac460a34e98
Atamanenko Konstantin, SUB-SDC-NL-DOS-BLG <Konstantin.Atamanenko@swisscom.com>
2024-04-19 23:20:43 +0200

    test

--- test/integration/test_functions.py
+++ test/integration/test_functions.py
@@ -73,7 +73,7 @@ def display_image_8bpp(display):
 
     img = Image.open(img_path)
 
-    img = img.resample(angle=90,expand=True,reshape=Image.BICUBIC)
+    img = img.rotate(angle=90,expand=True,resample=Image.BICUBIC)
 
     # TODO: this should be built-in
     dims = (display.width, display.height)
