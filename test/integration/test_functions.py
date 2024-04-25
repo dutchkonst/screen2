af626c2698ebe4f71e203714635002356c9ccf85
Atamanenko Konstantin, SUB-SDC-NL-DOS-BLG <Konstantin.Atamanenko@swisscom.com>
2024-04-25 21:41:48 +0200

    changes

--- test/integration/test_functions.py
+++ test/integration/test_functions.py
@@ -137,24 +137,29 @@ def draw_line_1bit(display):
 
     w = display.epd.width;
     h = display.epd.height;
-    shape1 = [(40, 40), (w - 10, h - 10)] 
-    shape2 = [(50, 40), (w - 20, h - 10)] 
+    shape = [(40, 40), (w - 10, h - 10)] 
+     
+    for i in range(10):
+        oldshape = shape
 
-    # creating new Image object 
-    img = Image.new("1", (w, h), color="white");
+        # creating new Image object 
+        img = Image.new("1", (w, h), color="white");
     
-    # create line image 
-    img1 = ImageDraw.Draw(img)   
-    img1.line(shape1, fill ="black", width = 3) 
+        # create line image 
+        img1 = ImageDraw.Draw(img)   
+        img1.line(shape1, fill ="black", width = 3) 
 
-    display.frame_buf.paste(img)
-    display.draw_full(constants.DisplayModes.DU)
+        display.frame_buf.paste(img)
+        display.draw_full(constants.DisplayModes.DU)
 
-    img1.line(shape1, fill ="white", width = 3) 
-    img1.line(shape2, fill ="black", width = 3) 
+        img1.line(shape, fill ="white", width = 3) 
 
-    display.frame_buf.paste(img)
-    display.draw_full(constants.DisplayModes.DU)
+        shape = [(40 + 10 * (i+1), 40), (w - (10 * (i+1)), h - 10)]
+
+        img1.line(shape2, fill ="black", width = 3) 
+
+        display.frame_buf.paste(img)
+        display.draw_full(constants.DisplayModes.DU)
     
     
 
