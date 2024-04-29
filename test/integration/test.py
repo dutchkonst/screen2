f115d28af10c0d7c9591491df9ab94711710ab95
Atamanenko Konstantin, SUB-SDC-NL-DOS-BLG <Konstantin.Atamanenko@swisscom.com>
2024-04-29 21:59:56 +0200

    test

--- test/integration/test.py
+++ test/integration/test.py
@@ -31,7 +31,7 @@ def main():
         # value means faster display refreshes. the documentation for the IT8951 device
         # says the max is 24 MHz (24000000), but my device seems to still work as high as
         # 80 MHz (80000000)
-        display = AutoEPDDisplay(vcom=-2.15, rotate=args.rotate, mirror=args.mirror, spi_hz=24000000)
+        display = AutoEPDDisplay(vcom=-2.15, rotate=args.rotate, mirror=args.mirror, spi_hz=50000000)
 
         print('VCOM set to', display.epd.get_vcom())
 
